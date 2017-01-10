1ba58c82defe0c5662d1db26a7a324085623dba3
Yamadads <spawlak@yahoo.com>
2017-01-10 20:24:19 +0100

    successMessages tests v2

---
+++ successMessagev2.sh
@@ -0,0 +1,6 @@
+DIR="./PrometheeAlternativesProfiles/"
+
+  find "${DIR}" -name 'messages.xml' -print | while read f; do
+    sed -i -e 's,    <methodMessages/>,    <methodMessages>\n        <logMessage>\n            <text>Success</text>\n        </logMessage>\n    </methodMessages>\n,g' "$f";
+    
+  done
