8a89e3a20bbbddbb2dbf2053735d06a3021429d2
Yamadads <spawlak@yahoo.com>
2017-01-10 20:30:47 +0100

    success messages in v2 output files

--- successMessagev2.sh
+++ successMessagev2.sh
@@ -1,4 +1,4 @@
-DIR="./PrometheeAlternativesProfiles/"
+DIR="./"
 
   find "${DIR}" -name 'messages.xml' -print | while read f; do
     sed -i -e 's,    <methodMessages/>,    <methodMessages>\n        <logMessage>\n            <text>Success</text>\n        </logMessage>\n    </methodMessages>\n,g' "$f";
