698a55fdff1d1b10464a207dd36e5b1c896e9774
Yamadads <spawlak@yahoo.com>
2017-03-02 17:24:08 +0100

    update tests preference discordance

--- newVersion.sh
+++ newVersion.sh
@@ -1,4 +1,4 @@
-DIR="./PROMETHEE_preference-reinforcedPreference/"
+DIR="./PROMETHEE_preferenceDiscordance/"
 
   # use "-name '*.java'" instead of "-type f" if you want to match java source files only
 
